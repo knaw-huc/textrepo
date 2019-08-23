5de766175d30406e6f4b5e41f7e7a11d2ceeeaee
HDJ <hayco@users.noreply.github.com>
2019-08-23 11:08:31 +0200

    Cleanup unused stuff

--- concordion/src/test/java/nl/knaw/huc/textrepo/TestFileHandling.java
+++ concordion/src/test/java/nl/knaw/huc/textrepo/TestFileHandling.java
@@ -6,19 +6,12 @@ import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 
 @RunWith(ConcordionRunner.class)
 public class TestFileHandling {
-  private final Logger logger = LoggerFactory.getLogger(TestFileHandling.class);
-
-  private static final String sha224 = "55d4c44f5bc05762d8807f75f3f24b4095afa583ef70ac97eaf7afc6";
-  // private static final String content = "hello test";
-
   public static String responsePart(Response response, String s) {
     return JsonPath.parse(response.readEntity(String.class)).read(s);
   }
@@ -35,9 +28,6 @@ public class TestFileHandling {
 
     var response = request.post(entity);
     final var sha224 = responsePart(response, "$.sha224");
-    // logger.info("response.status: {}", response.getStatus());
-    // logger.info("response.location: {}", response.getHeaderString("Location"));
-    // logger.info("response.body.sha224: {}", sha224);
 
     final Result result = new Result();
     result.status = response.getStatus();
