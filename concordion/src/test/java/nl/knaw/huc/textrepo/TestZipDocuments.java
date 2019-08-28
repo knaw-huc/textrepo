9caeeb8f4e090acccad63034c7e43a4ad4a51bb9
BasLee <basleenknegt@gmail.com>
2019-08-28 17:29:14 +0200

    Add first version of zip concordion test

--- concordion/src/test/java/nl/knaw/huc/textrepo/TestZipDocuments.java
+++ concordion/src/test/java/nl/knaw/huc/textrepo/TestZipDocuments.java
@@ -2,6 +2,7 @@ package nl.knaw.huc.textrepo;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
@@ -13,19 +14,29 @@ import java.io.IOException;
 import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM_TYPE;
-import static nl.knaw.huc.textrepo.Config.HTTP_APP_HOST;
 import static nl.knaw.huc.textrepo.TestUtils.getResourceFileBits;
 
 public class TestZipDocuments extends AbstractConcordionTest {
-  private static final String HOST = HTTP_APP_HOST;
 
   public static class TestZipDocumentsResult {
     public int status;
     public String locationCount;
+
     public String filename1;
     public String location1;
     public String documentIdIsUUID1;
     public String documentId1;
+
+    public String filename2;
+    public String location2;
+    public String documentIdIsUUID2;
+    public String documentId2;
+
+    public String fileHash1;
+    public String fileHash2;
+
+    public String getIndexDocument1;
+    public String getIndexDocument2;
   }
 
   public TestZipDocumentsResult uploadZip(String filename) throws IOException {
@@ -40,11 +51,42 @@ public class TestZipDocuments extends AbstractConcordionTest {
     var locations = getLocationsMap(body);
 
     result.locationCount = "" + locations.keySet().size();
+
     result.filename1 = "een.txt";
     result.location1 = locations.get(result.filename1);
     result.documentId1 = TestUtils.getDocumentId(result.location1);
     result.documentIdIsUUID1 = TestUtils.isValidUUID(result.documentId1);
+
+    result.filename2 = "twee.txt";
+    result.location2 = locations.get(result.filename2);
+    result.documentId2 = TestUtils.getDocumentId(result.location2);
+    result.documentIdIsUUID2 = TestUtils.isValidUUID(result.documentId2);
+
+    result.fileHash1 = getLatestVersionHash(result.documentId1);
+    result.fileHash2 = getLatestVersionHash(result.documentId2);
+
+    result.getIndexDocument1 = getIndexDocument(result.documentId1);
+    result.getIndexDocument2 = getIndexDocument(result.documentId2);
+
     return result;
+  }
+
+  private String getLatestVersionHash(String documentId) {
+    var url = APP_HOST + "/documents/" + documentId;
+    return JsonPath.parse(getByUrl(url)).read("$.fileSha");
+  }
+
+  private String getIndexDocument(String documentId) {
+    String url = ES_HOST + "/documents/_doc/" + documentId;
+    return JsonPath.parse(getByUrl(url)).read("$._source.content");
+  }
+
+  private String getByUrl(String url) {
+    return client()
+        .target(url)
+        .request()
+        .get()
+        .readEntity(String.class);
   }
 
   /**
--- concordion/src/test/resources/nl/knaw/huc/textrepo/TestZipDocuments.md
+++ concordion/src/test/resources/nl/knaw/huc/textrepo/TestZipDocuments.md
@@ -1,13 +1,19 @@
-# TestDocuments
+# TestZipDocuments
 
-## [Uploading a document](- 'upload')
+## [Uploading a zip file](- 'upload')
 
-When a zip file "[multiple-files.zip](- "#text")" with multiple files is [```POST```ed](- "#doc=uploadZip(#text)") then
+When a zip file "[multiple-files.zip](- "#text")" with two files is [```POST```ed](- "#doc=uploadZip(#text)") then
 
  - The HTTP response code should be [200](- "?=#doc.status");
  - The json response should contain [2](- "?=#doc.locationCount") location entries
  - The first entry should have filename [een.txt](- "?=#doc.filename1") 
  - The first entry should have a location with a [valid UUID](- "?=#doc.documentIdIsUUID1"): [ ](- "c:echo=#doc.location1").
+ - When requesting the latest version, the first document version should have fileHash: [bf83caeb324ec7d185e8bf266d97ca9354732b76eee46b6227adb082](- "?=#doc.fileHash1") 
+ - When querying the documents index, the first entry should have content: [Een.](- "?=#doc.getIndexDocument1") 
+ - The second entry should have filename [twee.txt](- "?=#doc.filename2") 
+ - The second entry should have a location with a [valid UUID](- "?=#doc.documentIdIsUUID2"): [ ](- "c:echo=#doc.location2").
+ - When requesting the latest version, the second document version should have a fileHash: [4fcc5672305c87f66f09c39f7229fd126cab95edf30821ec9ee4a66f](- "?=#doc.fileHash2") 
+ - When querying the documents index, the second entry should have content: [Twee.](- "?=#doc.getIndexDocument2") 
  
  
  
