931acd94e0de3e9bb8c6db31f95576fc00332a46
BasLee <basleenknegt@gmail.com>
2019-08-29 13:12:35 +0200

    Remove python integration project

--- integration/test/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-HTTP_ES_HOST = 'http://' + os.environ['ES_HOST']
-HTTP_APP_HOST = 'http://' + os.environ['APP_HOST']
-
-POSTGRES_HOST = os.environ['POSTGRES_HOST']
-POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
-POSTGRES_DB = os.environ['POSTGRES_DB']
-POSTGRES_USER = os.environ['POSTGRES_USER']
