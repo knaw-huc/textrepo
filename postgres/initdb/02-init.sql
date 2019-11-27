30df7430a695549f57135b912d94111f85342b21
HDJ <hayco@users.noreply.github.com>
2019-11-27 11:11:59 +0100

    Add typed files and documents

--- postgres/initdb/02-init.sql
+++ postgres/initdb/02-init.sql
@@ -1,28 +1,52 @@
 -- Contents of a file is identified by its SHA-224.
 create table contents (
-  sha224 char(56) not null constraint contents_pkey primary key,
+  sha224 char(56) primary key,
   content bytea
 );
 
--- FILES
--- implicit; create view to support, e.g., 'select * from files'
+-- Types of files in use (e.g., FoLia, Alto, PageXML, TEI, ...)
+create table types (
+  id serial primary key,
+  name varchar (16) not null unique
+);
+
+-- A file has a type and versioned contents
+create table files (
+  id uuid primary key,
+  type_id serial not null,
+  foreign key (type_id) references types (id)
+);
 
 -- A version is the contents of a file at a specific time.
 create table versions (
-  file_uuid uuid not null,
-  date timestamp with time zone not null,
+  file_id uuid not null,
   contents_sha char(56),
-  primary key (file_uuid, date),
+  created_at timestamp with time zone not null,
+  primary key (file_id, created_at),
+  foreign key (file_id) references files (id),
   foreign key (contents_sha) references contents (sha224)
 );
 
-create index version_by_uuid on versions (file_uuid);
+create index version_by_file_id on versions (file_id);
+
+-- A document may consist of several files and metadata, all are optional
+create table documents (
+  id uuid primary key
+);
+
+create table document_files (
+  document_id uuid not null,
+  file_id uuid not null,
+  foreign key (document_id) references documents (id),
+  foreign key (file_id) references files (id)
+);
 
 -- Document metadata items. Each item is a key-value pair linked to a
 -- file.
-create table metadata (
-  file_uuid uuid not null,
+create table document_metadata (
+  document_id uuid not null,
   key varchar not null,
   value text,
-  primary key (file_uuid, key)
+  primary key (document_id, key),
+  foreign key (document_id) references documents (id)
 );
