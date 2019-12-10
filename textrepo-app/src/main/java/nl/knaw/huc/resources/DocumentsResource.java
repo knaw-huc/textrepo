7d38cc35fd52869b3b78d14dec515fe877bc41dc
HDJ <hayco@users.noreply.github.com>
2019-12-10 10:45:56 +0100

    Add GET documents/{uuid}/{type}

--- textrepo-app/src/main/java/nl/knaw/huc/resources/DocumentsResource.java
+++ textrepo-app/src/main/java/nl/knaw/huc/resources/DocumentsResource.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Nonnull;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -43,24 +44,25 @@ public class DocumentsResource {
     this.fileService = fileService;
   }
 
+  @POST
+  // E.g., POST <new contents> to /documents/ed311646-1b2d-11ea-a290-53729ef3c030?type=FoLiA
   @Consumes(MULTIPART_FORM_DATA)
   @Produces(APPLICATION_JSON)
-  @POST
   @ApiOperation(value = "Create a new document by uploading contents for one of its file types")
   @ApiResponses(value = {@ApiResponse(code = 201, message = "CREATED")})
   public Response addDocument(
-    @QueryParam("type") @Nonnull String type,
-    @FormDataParam("contents") InputStream uploadedInputStream,
-    @FormDataParam("contents") FormDataContentDisposition fileDetail,
-    @FormDataParam("contents") FormDataBodyPart bodyPart
+      @QueryParam("type") @Nonnull String type,
+      @FormDataParam("contents") InputStream uploadedInputStream,
+      @FormDataParam("contents") FormDataContentDisposition fileDetail,
+      @FormDataParam("contents") FormDataBodyPart bodyPart
   ) {
     final var newFileId = fileService.createFile(type);
     LOG.debug("New file created with fileId={}", newFileId);
 
     fileService.createVersionWithFilenameMetadata(
-      newFileId,
-      readContent(uploadedInputStream),
-      fileDetail.getFileName());
+        newFileId,
+        readContent(uploadedInputStream),
+        fileDetail.getFileName());
     LOG.debug("New version of {} created for content", newFileId);
 
     final var docId = documentService.createDocument(newFileId);
@@ -68,16 +70,34 @@ public class DocumentsResource {
     return Response.created(locationOf(docId)).build();
   }
 
-  @Consumes(MULTIPART_FORM_DATA)
+  @GET
+  @Path("/{uuid}/{type}") // for now '/latest' is implicit
+  // E.g., GET /documents/ed311646-1b2d-11ea-a290-53729ef3c030/FoLiA
   @Produces(APPLICATION_JSON)
+  @ApiOperation(value = "Get latest version of document's file by type")
+  @ApiResponses(value = {@ApiResponse(code = 200, response = Version.class, message = "OK")})
+  public Response getLatestVersionByType(@PathParam("uuid") @Valid UUID docId,
+                                         @PathParam("type") @Nonnull String typeName) {
+    LOG.debug("getLatestVersionByType: docId={}, typeName={}", docId, typeName);
+    final var fileId = documentService.findFileForType(docId, typeName);
+
+    LOG.debug(" -> getting latest version of file: {}", fileId);
+    var version = fileService.getLatestVersion(fileId);
+
+    return Response.ok(version).build();
+  }
+
   @PUT
   @Path("/{uuid}/{type}")
+  // E.g., PUT <new contents> to /documents/ed311646-1b2d-11ea-a290-53729ef3c030/FoLiA
+  @Consumes(MULTIPART_FORM_DATA)
+  @Produces(APPLICATION_JSON)
   public Response updateDocumentByType(
-    @PathParam("uuid") @Valid UUID docId,
-    @PathParam("type") @Nonnull String type,
-    @FormDataParam("contents") InputStream uploadedInputStream,
-    @FormDataParam("contents") FormDataContentDisposition fileDetail,
-    @FormDataParam("contents") FormDataBodyPart bodyPart
+      @PathParam("uuid") @Valid UUID docId,
+      @PathParam("type") @Nonnull String type,
+      @FormDataParam("contents") InputStream uploadedInputStream,
+      @FormDataParam("contents") FormDataContentDisposition fileDetail,
+      @FormDataParam("contents") FormDataBodyPart bodyPart
   ) {
     LOG.debug("Updating {} contents of document: {}", type, docId);
 
@@ -85,9 +105,9 @@ public class DocumentsResource {
     LOG.debug(" -> updating file: {}", fileId);
 
     final var version = fileService.createVersionWithFilenameMetadata(
-      fileId,
-      readContent(uploadedInputStream),
-      fileDetail.getFileName());
+        fileId,
+        readContent(uploadedInputStream),
+        fileDetail.getFileName());
     LOG.debug(" -> new version created: {}", version);
 
     return Response.ok().build();
