733837e83c893844de4fd640580636624ce0198d
HDJ <hayco@users.noreply.github.com>
2019-12-11 15:09:16 +0100

    Fix MetadataDao works on files
    TODO: rename?

--- textrepo-app/src/main/java/nl/knaw/huc/db/MetadataDao.java
+++ textrepo-app/src/main/java/nl/knaw/huc/db/MetadataDao.java
@@ -17,23 +17,23 @@ import java.util.UUID;
 
 public interface MetadataDao {
 
-  @SqlUpdate("insert into documents_metadata (document_id, key, value) values (:id, :key, :value)")
+  @SqlUpdate("insert into files_metadata (file_id, key, value) values (:id, :key, :value)")
   void insert(@Bind("id") UUID fileId, @BindBean MetadataEntry metadataEntry);
 
   @Transaction
-  @SqlUpdate("update documents_metadata set value = :value where document_id = :id and key = :key ")
+  @SqlUpdate("update files_metadata set value = :value where file_id = :id and key = :key ")
   void update(@Bind("id") UUID fileId, @BindBean MetadataEntry metadataEntry);
 
   @Transaction
-  @SqlBatch("insert into documents_metadata (document_id, key, value) values (:id, :key, :value)")
+  @SqlBatch("insert into files_metadata (file_id, key, value) values (:id, :key, :value)")
   @BatchChunkSize(1000)
   void bulkInsert(@Bind("id") UUID documentId, @BindBean Iterator<Map.Entry<String, String>> entries);
 
-  @SqlQuery("select document_id, key, value from documents_metadata where document_id = ? and key = ?")
+  @SqlQuery("select file_id, key, value from files_metadata where file_id = ? and key = ?")
   @RegisterConstructorMapper(MetadataEntry.class)
   Optional<MetadataEntry> findByDocumentIdAndKey(@Bind UUID documentId, @Bind String key);
 
-  @SqlQuery("select document_id, key, value from documents_metadata where document_id = ?")
+  @SqlQuery("select file_id, key, value from files_metadata where file_id = ?")
   @RegisterConstructorMapper(MetadataEntry.class)
   Iterator<MetadataEntry> findByDocumentId(@Bind UUID documentId);
 
