7c56fcb529ad5284b37f88c27fc69842cc8bfd80
HDJ <hayco@users.noreply.github.com>
2020-01-24 15:28:55 +0100

    Cleanup existing tables/indices
    Drop in reverse order of creation to ensure constraints are dropped
    before the tables that those constraints depend on, are removed.

--- postgres/initdb/01-clear.sql
+++ postgres/initdb/01-clear.sql
@@ -1,4 +1,15 @@
-drop index if exists version_by_uuid;
-drop table if exists contents;
+-- Cleanup existing database, dropping all tables and indices
+
+-- Note Drop in reverse order of creation to ensure constraints are dropped
+-- before the tables that those constraints depend on, are removed.
+
+drop table if exists files_metadata;
+drop table if exists documents_metadata;
+drop index if exists doc_files_by_file_id;
+drop table if exists documents_files;
+drop index if exists version_by_file_id;
 drop table if exists versions;
-drop table if exists metadata;
+drop index if exists files_by_type_id;
+drop table if exists files;
+drop table if exists types;
+drop table if exists contents;
