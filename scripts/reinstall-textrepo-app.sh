9da79c8e0c3c0b69218d13a4574e830fe51d9dff
BasLee <basleenknegt@gmail.com>
2020-01-30 12:05:26 +0100

    Add dev script to speed up reinstalling textrepo-app

---
+++ scripts/reinstall-textrepo-app.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# use for development purposes only
+
+# create new jar
+(cd textrepo-app && mvn clean install)
+
+# copy jar into running container
+docker cp ./textrepo-app/target/textrepo-1.0-SNAPSHOT.jar tr_textrepo:/textrepo-app/textrepo.jar
+docker cp ./textrepo-app/config.yml tr_textrepo:/textrepo-app/config.yml
+
+# save container with new jar as new image
+docker commit tr_textrepo knawhuc/textrepo-app:latest
+
+# restart container with new image
+docker-compose stop textrepo-app
+docker-compose up -d --no-deps textrepo-app
