c4222cc8a2e7089576e20143989bb6788c4d6ffa
BasLee <basleenknegt@gmail.com>
2020-02-07 14:22:45 +0100

    Update populate.sh

--- scripts/populate.sh
+++ scripts/populate.sh
@@ -2,18 +2,24 @@
 set -e
 
 HOST=localhost:8080/textrepo/rest
+OLD_CONTENT=./scripts/example.old.txt
+NEW_CONTENT=./scripts/example.new.txt
+
+# Check example files exist:
+if [[ ! -f $OLD_CONTENT ]] ; then echo "File '$OLD_CONTENT' not found, aborting."; exit; fi
+if [[ ! -f $NEW_CONTENT ]] ; then echo "File '$NEW_CONTENT' not found, aborting."; exit; fi
 
 
 # Create text and xml file types:
 
 TEXT_TYPE_ID=$(curl "$HOST/types" \
   -H 'content-type:application/json' \
-  -d '{"name": "text", "mimetype": "text/plain"}' | jq '.id')
+  -d '{"name": "text", "mimetype": "text/plain"}' | jq -r '.id')
 echo "Created text type with id: $TEXT_TYPE_ID"
 
 XML_TYPE_ID=$(curl "$HOST/types" \
   -H 'content-type:application/json' \
-  -d '{"name": "xml", "mimetype": "application/xml"}' | jq '.id')
+  -d '{"name": "xml", "mimetype": "application/xml"}' | jq -r '.id')
 echo "Created xml type with id: $XML_TYPE_ID"
 
 
@@ -21,7 +27,7 @@ echo "Created xml type with id: $XML_TYPE_ID"
 
 DOC_ID=$(curl "$HOST/documents" \
   -H 'content-type:application/json' \
-  -d '{"externalId": "example-external-id"}' | jq '.id')
+  -d '{"externalId": "example-external-id"}' | jq -r '.id')
 echo "Created document with id: $DOC_ID"
 
 curl "$HOST/documents" \
@@ -33,10 +39,24 @@ curl "$HOST/documents" \
 
 TEXT_FILE_ID=$(curl "$HOST/files" \
   -H 'content-type:application/json' \
-  -d "{\"docId\": $DOC_ID, \"typeId\": $TEXT_TYPE_ID}" | jq '.id')
+  -d "{\"docId\": \"$DOC_ID\", \"typeId\": \"$TEXT_TYPE_ID\"}" | jq -r '.id')
 echo "Created text file with id: $TEXT_FILE_ID"
 
 XML_FILE_ID=$(curl "$HOST/files" \
   -H 'content-type:application/json' \
-  -d "{\"docId\": $DOC_ID, \"typeId\": $XML_TYPE_ID}" | jq '.id')
+  -d "{\"docId\": \"$DOC_ID\", \"typeId\": \"$XML_TYPE_ID\"}" | jq -r '.id')
 echo "Created xml file with id: $XML_FILE_ID"
+
+
+# Add two versions to text file:
+
+OLD_VERSION_ID=$(curl "$HOST/versions" \
+  -F "fileId=$TEXT_FILE_ID" \
+  -F "contents=@$OLD_CONTENT" | jq -r '.id')
+echo "Created old version with id: $OLD_VERSION_ID"
+
+NEW_VERSION_ID=$(curl "$HOST/versions" \
+  -F "fileId=$TEXT_FILE_ID" \
+  -F "contents=@$NEW_CONTENT" | jq -r '.id')
+echo "Created new version with id: $NEW_VERSION_ID"
+
