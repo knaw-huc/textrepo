e8747b3c9a944827b28ddaa4754d95b9aad04d8b
HDJ <hayco@users.noreply.github.com>
2020-02-18 12:14:14 +0100

    Migrate to JUnit 5

--- textrepo-app/src/test/java/nl/knaw/huc/service/task/FindDocumentByExternalIdTest.java
+++ textrepo-app/src/test/java/nl/knaw/huc/service/task/FindDocumentByExternalIdTest.java
@@ -3,15 +3,16 @@ package nl.knaw.huc.service.task;
 import nl.knaw.huc.core.Document;
 import nl.knaw.huc.db.DocumentsDao;
 import org.jdbi.v3.core.Handle;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.NotFoundException;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
@@ -24,12 +25,12 @@ public class FindDocumentByExternalIdTest {
   private static final String TEST_EXTERNAL_ID = "/some/external/ID{maybe=0xCAFEBABE}";
   private static final Document TEST_DOCUMENT = new Document(UUID.randomUUID(), TEST_EXTERNAL_ID);
 
-  @Before
+  @BeforeEach
   public void setUp() {
     when(TRANSACTION.attach(DocumentsDao.class)).thenReturn(DOCUMENTS_DAO);
   }
 
-  @After
+  @AfterEach
   public void resetMocks() {
     reset(DOCUMENTS_DAO, TRANSACTION);
   }
@@ -47,9 +48,10 @@ public class FindDocumentByExternalIdTest {
     assertThat(new FindDocumentByExternalId(TEST_EXTERNAL_ID).executeIn(TRANSACTION)).isEqualTo(TEST_DOCUMENT);
   }
 
-  @Test(expected = NotFoundException.class)
+  @Test
   public void testFindDocumentByExternalId_throwsNotFound_whenDocumentNotFound() {
     when(DOCUMENTS_DAO.getByExternalId(any())).thenReturn(Optional.empty());
-    new FindDocumentByExternalId(TEST_EXTERNAL_ID).executeIn(TRANSACTION);
+    assertThrows(NotFoundException.class, () ->
+        new FindDocumentByExternalId(TEST_EXTERNAL_ID).executeIn(TRANSACTION));
   }
 }
