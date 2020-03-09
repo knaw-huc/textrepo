b4814ab63e78339da75c38073430d024756db4f4
basl <basleenknegt@gmail.com>
2020-03-09 15:37:33 +0100

    Remove unused DocumentFilesDao methods

--- textrepo-app/src/main/java/nl/knaw/huc/db/DocumentFilesDao.java
+++ textrepo-app/src/main/java/nl/knaw/huc/db/DocumentFilesDao.java
@@ -16,21 +16,10 @@ public interface DocumentFilesDao {
   @SqlUpdate("insert into documents_files (document_id, file_id) values (:docId, :fileId)")
   void insert(@Bind("docId") UUID docId, @Bind("fileId") UUID fileId);
 
-  @SqlQuery("SELECT f.id, f.type_id FROM files f, documents_files df, types t " +
-      "WHERE df.document_id = :docId " +
-      "AND t.name = :typeName " +
-      "AND f.id = df.file_id " +
-      "AND t.id = f.type_id")
-  @RegisterConstructorMapper(TextrepoFile.class)
-  Optional<TextrepoFile> findFile(@Bind("docId") UUID docId, @Bind("typeName") String typeName);
-
   @SqlQuery("SELECT f.id, f.type_id FROM files f, documents_files df " +
       "WHERE df.document_id = :docId AND f.id = df.file_id AND f.type_id = :typeId")
   @RegisterConstructorMapper(TextrepoFile.class)
   Optional<TextrepoFile> findFile(@Bind("docId") UUID docId, @Bind("typeId") short typeId);
-
-  @SqlQuery("select distinct id from documents where external_id = :externalId")
-  Optional<UUID> findDocumentByExternalId(@Bind("externalId") String externalId);
 
   @SqlQuery("select distinct document_id from documents_files where file_id = :fileId")
   Optional<UUID> findDocumentId(@Bind("fileId") UUID fileId);
