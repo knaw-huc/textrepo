3e25972f499a13d177da6abe573eaf13de0237bf
basl <basleenknegt@gmail.com>
2020-03-12 12:30:55 +0100

    Update autocomplete indexer concordion test

--- concordion/src/test/java/nl/knaw/huc/textrepo/TestCustomIndexer.java
+++ concordion/src/test/java/nl/knaw/huc/textrepo/TestCustomIndexer.java
@@ -1,92 +1,100 @@
 package nl.knaw.huc.textrepo;
 
 import com.jayway.jsonpath.JsonPath;
-import nl.knaw.huc.textrepo.util.TestUtils;
-import org.concordion.api.MultiValueResult;
+import nl.knaw.huc.textrepo.util.RestUtils;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
-
-import javax.ws.rs.client.Client;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.UUID;
 
 import static javax.ws.rs.client.Entity.entity;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 import static nl.knaw.huc.textrepo.Config.AUTOCOMPLETE_INDEX;
-import static nl.knaw.huc.textrepo.Config.FILES_URL;
 import static nl.knaw.huc.textrepo.util.IndexUtils.indexToUrl;
 import static nl.knaw.huc.textrepo.util.IndexUtils.refreshIndex;
-import static nl.knaw.huc.textrepo.util.TestUtils.getLocation;
+import static nl.knaw.huc.textrepo.util.TestUtils.asPrettyJson;
 import static nl.knaw.huc.textrepo.util.TestUtils.isValidUuid;
-import static nl.knaw.huc.textrepo.util.TestUtils.postFileWithFilename;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 public class TestCustomIndexer extends AbstractConcordionTest {
+
+  private final String esQuery = "{\n" +
+      "  \"suggest\": {\n" +
+      "    \"keyword-suggest\": {\n" +
+      "      \"prefix\":\"%prefix%\",\n" +
+      "      \"completion\": {\n" +
+      "        \"field\": \"suggest\",\n" +
+      "        \"skip_duplicates\": true,\n" +
+      "        \"size\": 5\n" +
+      "      }\n" +
+      "    }\n" +
+      "  },\n" +
+      "  \"_source\": \"suggest\"\n" +
+      "}";
+
+  public String createDocument() {
+    return RestUtils.createDocument("test-" + randomAlphabetic(5));
+  }
+
+  public String createFile(String docId) {
+    return RestUtils.createFile(docId, textTypeId);
+  }
 
   public static class UploadResult {
     public String validUuid1;
     public String validUuid2;
     public String validUuid3;
-    public String validUuids;
+    public String validVersions;
   }
 
-  public UploadResult upload(String content1, String content2, String content3) throws MalformedURLException {
+  public UploadResult upload(
+      String fileId1, String content1,
+      String fileId2, String content2,
+      String fileId3, String content3
+  ) {
     var result = new UploadResult();
-    result.validUuid1 = uploadFile(content1, client());
-    result.validUuid2 = uploadFile(content2, client());
-    result.validUuid3 = uploadFile(content3, client());
-    result.validUuids =
+    result.validUuid1 = RestUtils.createVersion(fileId1, content1);
+    result.validUuid2 = RestUtils.createVersion(fileId2, content2);
+    result.validUuid3 = RestUtils.createVersion(fileId3, content3);
+    result.validVersions =
         isValidUuid(result.validUuid1) && isValidUuid(result.validUuid2) && isValidUuid(result.validUuid3)
-            ? "valid UUIDs"
+            ? "valid versions"
             : "one or more invalid UUIDs";
     return result;
   }
 
-  public MultiValueResult searchAutocomplete(String pre) {
+  public static class AutocompleteResult {
+    public String suggestion1;
+    public String suggestion2;
+    public String suggestion3;
+    public String body;
+  }
+
+  public AutocompleteResult searchAutocomplete(String prefix) {
     refreshIndex(client(), AUTOCOMPLETE_INDEX);
 
-    var query = "{" +
-        "  \"suggest\": {" +
-        "    \"keyword-suggest\": {" +
-        "      \"prefix\": \"" + pre + "\"," +
-        "      \"completion\": {" +
-        "        \"field\": \"suggest\"," +
-        "        \"skip_duplicates\": true," +
-        "        \"size\": 5" +
-        "      }" +
-        "    }" +
-        "  }," +
-        "  \"_source\": \"suggest\"" +
-        "}";
+    var query = esQuery.replace("%prefix%", prefix);
 
-    var request = client()
+    var response = client()
         .register(MultiPartFeature.class)
         .target(indexToUrl(AUTOCOMPLETE_INDEX) + "/_search")
         .request()
         .post(entity(query, APPLICATION_JSON_TYPE));
 
-    var json = request.readEntity(String.class);
-    var parsedJson = JsonPath.parse(json);
+    var body = response.readEntity(String.class);
+    System.out.println("es body: " + body);
+    var parsedJson = JsonPath.parse(body);
     String suggestion1 = parsedJson.read("$.suggest['keyword-suggest'][0].options[0].text");
     String suggestion2 = parsedJson.read("$.suggest['keyword-suggest'][0].options[1].text");
     String suggestion3 = parsedJson.read("$.suggest['keyword-suggest'][0].options[2].text");
 
-    return new MultiValueResult()
-        .with("json", json)
-        .with("suggestion1", suggestion1)
-        .with("suggestion2", suggestion2)
-        .with("suggestion3", suggestion3);
+    var result = new AutocompleteResult();
+    result.suggestion1 = suggestion1;
+    result.suggestion2 = suggestion2;
+    result.suggestion3 = suggestion3;
+    result.body = asPrettyJson(body);
+    return result;
   }
 
-  private static String uploadFile(String content, Client client) throws MalformedURLException {
-    var filename = "test-" + UUID.randomUUID() + ".txt";
-    var endpoint = new URL(FILES_URL);
-    var response = postFileWithFilename(client, endpoint, filename, content.getBytes());
-
-    var locationHeader = getLocation(response);
-    var fileId = locationHeader
-        .map(TestUtils::getFileId)
-        .orElseThrow();
-    return fileId;
+  public String getEsQuery() {
+    return asPrettyJson(this.esQuery);
   }
 
 }
--- concordion/src/test/resources/nl/knaw/huc/textrepo/TestCustomIndexer.md
+++ concordion/src/test/resources/nl/knaw/huc/textrepo/TestCustomIndexer.md
@@ -1,26 +1,49 @@
 # TestCustomIndexer
 
-Testing the autocomplete indexer
+The autocomplete indexer indexes every file using elasticsearchs Completion Suggester. 
+New file versions will overwrite older versions in the es index. 
 
-## [Uploading files](- 'upload')
+To retrieve suggestions, we first create three document with three text files: 
+  
+[ ](- "#docId1=createDocument()")
+[ ](- "#fileId1=createFile(#docId1)")
+[ ](- "#docId2=createDocument()")
+[ ](- "#fileId2=createFile(#docId2)")
+[ ](- "#docId3=createDocument()")
+[ ](- "#fileId3=createFile(#docId3)")
 
-When files with contents 
+  - File ID: [ ](- "c:echo=#fileId1")
+  - File ID: [ ](- "c:echo=#fileId2")
+  - File ID: [ ](- "c:echo=#fileId3")  
+
+## Uploading file versions
+
+When new file versions with contents 
 "[hello Tesseract](- "#text1")", 
 "[hello teStament](- "#text2")" and 
 "[hello testosterone](- "#text3")" 
-are [```POST```ed](- "#file=upload(#text1, #text2, #text3)"),
-then we should have three [valid UUIDs](- "?=#file.validUuids"):
+are [```POST```ed](- "#versions=upload(#fileId1, #text1, #fileId2, #text2, #fileId3, #text3)"),
+then we should have three [valid versions](- "?=#versions.validVersions"):
 
-  - [ ](- "c:echo=#file.validUuid1")
-  - [ ](- "c:echo=#file.validUuid2")
-  - [ ](- "c:echo=#file.validUuid3")
+  - [ ](- "c:echo=#versions.validUuid1")
+  - [ ](- "c:echo=#versions.validUuid2")
+  - [ ](- "c:echo=#versions.validUuid3")
 
 ## [Searching with autocomplete](- 'searchAutocomplete')
 
-When "[tes](- "#pre")" 
-is [searched for](- "#suggestions=searchAutocomplete(#pre)") in the autocomplete index, 
-we get three suggestions: 
+When we search in the autocomplete index using the following query:
+
+[ ](- "ext:embed=getEsQuery()")
+
+  - where the `%prefix%` is: [`tes`](- "#prefix");
+
+[ ](- "#suggestions=searchAutocomplete(#prefix)")
+
+then we get three suggestions: 
 
   - [Tesseract](- "?=#suggestions.suggestion1") 
   - [teStament](- "?=#suggestions.suggestion2") 
   - [testosterone](- "?=#suggestions.suggestion3") 
+
+Full response:
+[ ](- "ext:embed=#suggestions.body")
