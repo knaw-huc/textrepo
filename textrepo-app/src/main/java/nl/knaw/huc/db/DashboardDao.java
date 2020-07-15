7bbd97865d1092918ee5f6409ea9cc49e8f6aa2c
HDJ <hayco@users.noreply.github.com>
2020-07-15 13:59:12 +0200

    Keep #countOrphans close to #findOrphans

--- textrepo-app/src/main/java/nl/knaw/huc/db/DashboardDao.java
+++ textrepo-app/src/main/java/nl/knaw/huc/db/DashboardDao.java
@@ -10,14 +10,14 @@ import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import java.util.List;
 
 public interface DashboardDao {
+  @SqlQuery("select * from get_documents_overview()")
+  @RegisterConstructorMapper(DocumentsOverview.class)
+  DocumentsOverview getDocumentsOverview();
+
   @SqlQuery("select count(*) from documents d where " +
       "not exists (select from documents_files fm where fm.document_id = d.id) " +
       "and not exists (select from documents_metadata dm where dm.document_id = d.id) ")
   int countOrphans();
-
-  @SqlQuery("select * from get_documents_overview()")
-  @RegisterConstructorMapper(DocumentsOverview.class)
-  DocumentsOverview getDocumentsOverview();
 
   @SqlQuery("select d.id, d.external_id, d.created_at from documents d where " +
       "not exists (select from documents_files fm where fm.document_id = d.id)" +
