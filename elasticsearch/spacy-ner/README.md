18d1d483cbfc8abc96b840a37e83d88fb2876043
basl <basleenknegt@gmail.com>
2020-07-16 16:34:27 +0200

    Add minimal spacy ner indexer

---
+++ elasticsearch/spacy-ner/README.md
@@ -0,0 +1,23 @@
+# NER Indexer
+
+Text repository indexer that recognizes named entities using spacy, flask and docker.
+
+To use: add spacy model to `./model`
+
+### Add service to Text Repository
+
+Add to docker-compose.yml:
+```
+
+```
+
+Add to docker-compose.env:
+```
+
+```
+
+Query elasticsearch example:
+
+```
+
+```
---
+++ elasticsearch/spacy-ner/py/app/router.py
@@ -0,0 +1,35 @@
+import sys
+
+import flask
+from flask import Response, request, jsonify, send_file
+from . import tagger
+
+app = flask.Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+ROOT = '/spacy-ner'
+
+@app.route(ROOT, methods=['GET'])
+def home():
+    resp = Response(
+        response='{"endpoints": ["GET /", "GET /mapping", "POST /fields"]}',
+        mimetype="application/json",
+        status=200
+    )
+    return resp
+
+
+@app.route(ROOT + '/mapping', methods=['GET'])
+def mapping():
+    return send_file('/tagger/mapping.json', 'application/json')
+
+
+@app.route(ROOT + '/fields', methods=['POST'])
+def tag():
+    text = request.form['file']
+    short = ((text[:98] + '..') if len(text) > 100 else text).replace('\n', ' ').replace('\r', ' ')
+    print('create fields of [' + short + ']', file=sys.stderr)
+    entities = tagger.tag(text)
+    return jsonify({'contents': text, 'entity': entities})
+
+
+app.run(host='0.0.0.0', port=8080, debug=False)
