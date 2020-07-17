d516090bf09a6e2bab7a772ca7fac2385f3ea564
HDJ <hayco@users.noreply.github.com>
2020-07-17 14:34:38 +0200

    Functions in lower case

--- textrepo-app/src/main/resources/nl/knaw/huc/db/DashboardDao/countOrphans.sql
+++ textrepo-app/src/main/resources/nl/knaw/huc/db/DashboardDao/countOrphans.sql
@@ -1,4 +1,4 @@
 SELECT count(*)
 FROM documents d
-WHERE NOT EXISTS(SELECT FROM documents_files fm WHERE fm.document_id = d.id)
-  AND NOT EXISTS(SELECT FROM documents_metadata dm WHERE dm.document_id = d.id)
+WHERE NOT exists(SELECT FROM documents_files fm WHERE fm.document_id = d.id)
+  AND NOT exists(SELECT FROM documents_metadata dm WHERE dm.document_id = d.id)
