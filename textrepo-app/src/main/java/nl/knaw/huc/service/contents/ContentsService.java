106da62afd5782baee28152e73b44c7c4d3cad9c
basl <basleenknegt@gmail.com>
2020-08-19 19:42:26 +0200

    Fix checkstyle warning

--- textrepo-app/src/main/java/nl/knaw/huc/service/contents/ContentsService.java
+++ textrepo-app/src/main/java/nl/knaw/huc/service/contents/ContentsService.java
@@ -27,8 +27,8 @@ public class ContentsService {
    *
    * <p>Like org.apache.commons.lang3.StringUtils.StringUtils.abbreviateMiddle
    * but then without having to convert byte[] to String
-   * <p>
-   * TODO: properly handle multi-byte UTF sequences (TT-606).
+   *
+   * <p>TODO: properly handle multi-byte UTF sequences (TT-606).
    */
   public static String abbreviateMiddle(@Nonnull byte[] contents) {
     var replacement = "[..]";
