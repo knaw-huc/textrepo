67172946f2050c9889ff9f8f5d3f5e9145e378b7
basl <basleenknegt@gmail.com>
2020-09-11 10:32:03 +0200

    Allow log.sh to tail logs (e.g. after large import)

--- examples/production/log.sh
+++ examples/production/log.sh
@@ -2,8 +2,9 @@
 set -e
 
 if [[ -z "$1" ]] || ([[ "$1" != 'prod' ]] && [[ "$1" != 'integration' ]]);
-  then echo 'usage: ./log.sh (prod|integration)'; exit;
+  then echo 'usage: ./log.sh (prod|integration) [<tail>]'; exit;
 fi
 
 source docker-compose.env
-docker-compose -f "docker-compose-${1}.yml" logs -f
+
+docker-compose -f "docker-compose-${1}.yml" logs -f --tail=${2:-all}
