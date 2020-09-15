288e5d95cc6d463f073e25f38d1b6c219ba2e542
HDJ <hayco@users.noreply.github.com>
2020-09-15 12:02:34 +0200

    Add minimal 'large objects' API

--- textrepo-app/src/main/java/nl/knaw/huc/db/LargeObjectsDao.java
+++ textrepo-app/src/main/java/nl/knaw/huc/db/LargeObjectsDao.java
@@ -1,4 +1,17 @@
 package nl.knaw.huc.db;
 
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.io.InputStream;
+
 public interface LargeObjectsDao {
+  @SqlQuery("insert into large_objects (lob) values (:data) returning *")
+  long insert(InputStream data);
+
+  @SqlQuery("select lob from large_objects where id = :id")
+  InputStream read(long id);
+
+  @SqlUpdate("delete from large_objects where id = :id returning lo_unlink(lob)")
+  void delete(long id);
 }
