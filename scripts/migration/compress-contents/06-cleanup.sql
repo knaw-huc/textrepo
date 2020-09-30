58fd873e8c659bb68d30f5ce27c42611ee618bb5
HDJ <hayco@users.noreply.github.com>
2020-09-30 09:39:41 +0200

    Add reference to 'vacuum full' postgres docs

--- scripts/migration/compress-contents/06-cleanup.sql
+++ scripts/migration/compress-contents/06-cleanup.sql
@@ -1,2 +1,6 @@
 -- reclaim diskspace after dropping column 'contents' by performing a 'full vacuum'
+--
+-- See https://www.postgresql.org/docs/9.1/sql-vacuum.html on how 'aggressive'
+-- VACUUM FULL is vs. a regular VACUUM
+
 VACUUM FULL
