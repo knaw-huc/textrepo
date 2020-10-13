086500b4663cfc63a11f7cf283596bee6690f46d
basl <basleenknegt@gmail.com>
2020-10-13 15:12:48 +0200

    Add contentDecompressionLimit (in kB) to About page

--- textrepo-app/src/main/java/nl/knaw/huc/api/ResultAbout.java
+++ textrepo-app/src/main/java/nl/knaw/huc/api/ResultAbout.java
@@ -8,10 +8,14 @@ import static java.util.stream.Collectors.toList;
 
 public class ResultAbout {
   private final ResultAboutVersion version;
+  private final String contentDecompressionLimit;
+
   private final List<ResultIndexer> indexers;
 
   public ResultAbout(TextRepoConfiguration config) {
     this.version = new ResultAboutVersion(config.getVersion());
+    this.contentDecompressionLimit = config.getResourceLimits().contentDecompressionLimit + "kB";
+
     this.indexers = config
         .getCustomFacetIndexers()
         .stream()
@@ -25,5 +29,9 @@ public class ResultAbout {
 
   public List<ResultIndexer> getIndexers() {
     return indexers;
+  }
+
+  public String getContentDecompressionLimit() {
+    return contentDecompressionLimit;
   }
 }
