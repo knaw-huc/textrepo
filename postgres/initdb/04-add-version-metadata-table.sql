8b3c567cbad365a085dc259691c8bab1d8f10f59
basl <basleenknegt@gmail.com>
2020-11-04 12:35:12 +0100

    Add postgres initdb script to create version_metadata table

---
+++ postgres/initdb/04-add-version-metadata-table.sql
@@ -0,0 +1,8 @@
+-- Version metadata: each entry is a key-value pair linked to a version.
+create table version_metadata (
+  version_id uuid not null,
+  key varchar not null,
+  value text,
+  primary key (version_id, key),
+  constraint versions_metadata_version_id_fkey foreign key (version_id) references versions (id) on delete cascade
+);
