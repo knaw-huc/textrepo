07ebc39b3c79c1a882658591d6fb07f30abc7531
HDJ <hayco@users.noreply.github.com>
2021-03-31 14:26:17 +0200

    Test importing document 404 and 201 casees

--- concordion/src/test/java/nl/knaw/huc/textrepo/task/TestImportDocument.java
+++ concordion/src/test/java/nl/knaw/huc/textrepo/task/TestImportDocument.java
@@ -1,9 +1,78 @@
 package nl.knaw.huc.textrepo.task;
 
-import org.concordion.integration.junit4.ConcordionRunner;
-import org.junit.runner.RunWith;
+import nl.knaw.huc.textrepo.AbstractConcordionTest;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
-@RunWith(ConcordionRunner.class)
-public class TestImportDocument {
+import javax.ws.rs.core.Response;
+import java.util.Optional;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Map.of;
+import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM_TYPE;
+import static nl.knaw.huc.textrepo.util.TestUtils.asCodeBlock;
+import static nl.knaw.huc.textrepo.util.TestUtils.asHeaderLink;
+import static nl.knaw.huc.textrepo.util.TestUtils.asPrettyJson;
+import static nl.knaw.huc.textrepo.util.TestUtils.replaceInUrlAndQueryParams;
+
+public class TestImportDocument extends AbstractConcordionTest {
+  public static class ImportResult {
+    public int status;
+    public String headers;
+    public String body;
+  }
+
+  public ImportResult retrieve(String endpoint,
+                               String externalId,
+                               String typeName,
+                               String content
+  ) {
+    final var response = importDocument(endpoint, externalId, content.getBytes(), typeName);
+
+    final var result = new ImportResult();
+    result.status = response.getStatus();
+
+    final var body = response.readEntity(String.class);
+    result.body = asPrettyJson(body.equals("") ? " " : body);
+
+    result.headers = "";
+    Optional.ofNullable(response.getHeaders().get("Link"))
+            .ifPresent(links -> links.forEach(l -> {
+              System.out.println("link:" + l.toString());
+              result.headers += asHeaderLink(l.toString());
+            }));
+    result.headers = asCodeBlock(result.headers);
+
+    return result;
+  }
+
+  private Response importDocument(String endpoint, String externalId, byte[] content, String typeName) {
+    var contentDisposition = FormDataContentDisposition
+        .name("contents")
+        .fileName(externalId + ".txt")
+        .size(content.length)
+        .build();
+
+    final var multiPart = new FormDataMultiPart()
+        .bodyPart(new FormDataBodyPart(
+            contentDisposition,
+            new String(content, UTF_8),
+            APPLICATION_OCTET_STREAM_TYPE)
+        );
+
+    var importUri = replaceInUrlAndQueryParams(endpoint, of(
+        "{externalId}", externalId,
+        "{typeName}", typeName));
+    var request = client
+        .register(MultiPartFeature.class)
+        .target(importUri)
+        .request();
+
+    var entity = entity(multiPart, multiPart.getMediaType());
+    return request.post(entity);
+
+  }
 }
--- concordion/src/test/resources/nl/knaw/huc/textrepo/task/TestImportDocument.md
+++ concordion/src/test/resources/nl/knaw/huc/textrepo/task/TestImportDocument.md
@@ -4,10 +4,10 @@ File contents can be directly uploaded to a document, referenced by its `externa
 version of a `typeName` typed file for that document.
 
 If the document with `externalId` does not yet exist, you can have the repository create it during import by passing
-`allowNewDocuments=true`.
+`allowNewDocument=true`.
 
-Otherwise, using `allowNewDocuments=false`, the import will verify that you are uploading to an already existing
-document, or deny the request. Note that this is also the default if you leave `allowNewDocuments` out.
+Otherwise, using `allowNewDocument=false`, the import will verify that you are uploading to an already existing
+document, or deny the request. Note that this is also the default if you leave `allowNewDocument` out.
 
 If you offer the same contents of a version already registered for this document during another import, by
 default (`asLatestVersion=false`), no new version will be created as it already exists for that particular document and
@@ -18,3 +18,47 @@ However, if you _do_ want to have an **earlier** version, that was superseded by
 **latest** version again, you can pass `asLatestVersion=true` with your import request. In the above example of
 versions `1-2-3-4` being in the repository, if you were to import the contents of `2` again with `asLatestVersion=true`,
 the versions in the repository will look like `1-2-3-4-2` designating version `2` as the latest version again.
+
+## Importing contents for a document that does not exist yet
+
+We try to import a document
+
+- with external ID: [`document_1234`](- "#externalId");
+- using type: [`text`](- "#typeName");
+- uploading a file containing [`example content`](- "#contents");
+
+### 1. Refusing new documents to be made
+
+When we `POST` the file to
+[`/task/import/documents/dochment_1234/text`](- "#importEndpoint")
+
+(or equivalently by explicitly appending the query param `?allowNewDocument=false`)
+
+[ ](- "#result=retrieve(#importEndpoint, #externalId, #typeName, #contents)")
+
+Then:
+
+- The response status should be: [404](- "?=#result.status");
+- Full response:
+
+[ ](- "ext:embed=#result.body")
+
+### 2. Allowing new documents to be made
+
+However, when we `POST` our file to
+
+[`/task/import/documents/document_1234/text?allowNewDocument=true`](- "#importEndpoint")
+
+[ ](- "#result=retrieve(#importEndpoint, #externalId, #typeName, #contents)")
+
+Then:
+
+- The response status should be: [201](- "?=#result.status");
+- Link headers:
+
+[ ](- "ext:embed=#result.headers")
+
+- Full response:
+
+[ ](- "ext:embed=#result.body")
+
