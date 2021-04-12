6c740282c2f3b41b82abc3faf050cc7f74607aff
HDJ <hayco@users.noreply.github.com>
2021-04-12 13:54:56 +0200

    Expand tests to include …/lines/$startOffset/$endOffset

--- textrepo-app/src/test/java/nl/knaw/huc/resources/view/TextViewerResourceTest.java
+++ textrepo-app/src/test/java/nl/knaw/huc/resources/view/TextViewerResourceTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import javax.ws.rs.core.Response;
 import java.util.stream.Stream;
 
+import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,8 +22,14 @@ class TextViewerResourceTest {
   private static final int CONTENT_DECOMPRESSION_LIMIT = 10;
   private static final ContentsHelper CONTENTS_HELPER = new ContentsHelper(CONTENT_DECOMPRESSION_LIMIT);
 
-  private static final String EXAMPLE = "0123456789abcdef";
-  private static final Contents CONTENTS = Contents.fromBytes(EXAMPLE.getBytes(UTF_8));
+  private static final String CHARS = "0123456789abcdef";
+  private static final String LINE = CHARS + "\n";
+  private static final String LINES = LINE + LINE;
+
+  // Note that we construct CONTENTS to be WITHOUT trailing '\n'
+  // Yet we expect the endpoint's to return normalised lines, including trailing '\n
+  // This test is written to expect that normalised behaviour.
+  private static final Contents CONTENTS = Contents.fromBytes((LINE + CHARS).getBytes(UTF_8));
 
   public static final ResourceExtension resource = ResourceExtension
       .builder()
@@ -33,27 +39,68 @@ class TextViewerResourceTest {
   @SuppressWarnings("unused") // used as @MethodSource
   private static Stream<Arguments> provideCasesForChars() {
     return Stream.of(
-        Arguments.of("/chars/-1/4", 400, ">= 0"),
-        Arguments.of("/chars/-1/full", 400, ">= 0"),
-        Arguments.of("/chars/4/16", 400, "<= 15"),
-        Arguments.of("/chars/full/16", 400, "<= 15"),
+        Arguments.of("/chars/-1/4", 400, ">= 0, or 'full'"),
+        Arguments.of("/chars/-1/full", 400, ">= 0, or 'full'"),
+        Arguments.of("/chars/4/1000", 400, "must be <="),
+        Arguments.of("/chars/full/1000", 400, "must be <="),
         Arguments.of("/chars/4/0", 400, "endOffset must be >= startOffset"),
         Arguments.of("/chars/8/4", 400, "endOffset must be >= startOffset"),
         Arguments.of("/chars/15/8", 400, "endOffset must be >= startOffset"),
+        Arguments.of("/chars/0/0", 200, "0"),
+        Arguments.of("/chars/15/15", 200, "f"),
+        Arguments.of("/chars/32/32", 200, "f"),
+        Arguments.of("/chars/33/33", 400, "must be <= 32"),
         Arguments.of("/chars/4/8", 200, "45678"),
         Arguments.of("/chars/0/4", 200, "01234"),
-        Arguments.of("/chars/4/15", 200, "456789abcdef"),
-        Arguments.of("/chars/0/full", 200, EXAMPLE),
-        Arguments.of("/chars/full/15", 200, EXAMPLE),
-        Arguments.of("/chars/full/full", 200, EXAMPLE)
+        Arguments.of("/chars/21/32", 200, "456789abcdef"),
+        Arguments.of("/chars/0/full", 200, CHARS + "\n" + CHARS),
+        Arguments.of(format("/chars/full/%d", (CHARS + "\n" + CHARS).length() - 1), 200, CHARS + "\n" + CHARS),
+        Arguments.of("/chars/full/full", 200, CHARS + "\n" + CHARS)
+    );
+  }
+
+  private static Stream<Arguments> provideCasesForLines() {
+    return Stream.of(
+        Arguments.of("/lines/-1/4", 400, ">= 0, or 'full'"),
+        Arguments.of("/lines/-1/full", 400, ">= 0, or 'full'"),
+        Arguments.of("/lines/4/0", 400, "endOffset must be >= startOffset"),
+        Arguments.of("/lines/8/4", 400, "endOffset must be >= startOffset"),
+        Arguments.of("/lines/15/8", 400, "endOffset must be >= startOffset"),
+        Arguments.of("/lines/0/2", 400, "must be <= 1"),
+        Arguments.of("/lines/0/0", 200, LINE),
+        Arguments.of("/lines/1/1", 200, LINE),
+        Arguments.of("/lines/0/1", 200, LINES),
+        Arguments.of("/lines/0/full", 200, LINES),
+        Arguments.of("/lines/full/1", 200, LINES),
+        Arguments.of("/lines/full/full", 200, LINES)
     );
   }
 
   @ParameterizedTest
   @MethodSource("provideCasesForChars")
   void getChars_returnsExpectedStatus_and_Contents(String uri, int statusCode, String expected) {
-    final var response = get(uri);
+    checkResponse(uri, statusCode, expected);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideCasesForLines")
+  void getLines_returnsExpectedStatus_and_Contents(String uri, int statusCode, String expected) {
+    checkResponse(uri, statusCode, expected);
+  }
+
+  @Test
+  void getRange() {
+  }
+
+  private void checkResponse(String uri, int statusCode, String expected) {
+    final var response = resource
+        .client()
+        .target(uri)
+        .request()
+        .get();
+
     assertThat(response.getStatus()).isEqualTo(statusCode);
+
     final var body = response.readEntity(String.class);
     if (statusCode == 200) {
       assertThat(body).isEqualTo(expected);
@@ -64,15 +111,4 @@ class TextViewerResourceTest {
     }
   }
 
-  private Response get(String uri) {
-    return resource.client().target(uri).request().get();
-  }
-
-  @Test
-  void getLines() {
-  }
-
-  @Test
-  void getRange() {
-  }
 }
