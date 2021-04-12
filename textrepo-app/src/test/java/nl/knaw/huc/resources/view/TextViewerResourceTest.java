ce8d595a8a92c7b2a2a8781b97051f05c57ab84a
HDJ <hayco@users.noreply.github.com>
2021-04-12 12:47:44 +0200

    Extract common test code to easily add more cases

--- textrepo-app/src/test/java/nl/knaw/huc/resources/view/TextViewerResourceTest.java
+++ textrepo-app/src/test/java/nl/knaw/huc/resources/view/TextViewerResourceTest.java
@@ -6,6 +6,12 @@ import nl.knaw.huc.core.Contents;
 import nl.knaw.huc.helpers.ContentsHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.ws.rs.core.Response;
+import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,68 +21,44 @@ class TextViewerResourceTest {
   private static final int CONTENT_DECOMPRESSION_LIMIT = 10;
   private static final ContentsHelper CONTENTS_HELPER = new ContentsHelper(CONTENT_DECOMPRESSION_LIMIT);
 
-  private static final Contents CONTENTS = Contents.fromBytes("0123456789abcdef".getBytes(UTF_8));
+  private static final String EXAMPLE = "0123456789abcdef";
+  private static final Contents CONTENTS = Contents.fromBytes(EXAMPLE.getBytes(UTF_8));
 
   public static final ResourceExtension resource = ResourceExtension
       .builder()
       .addResource(new TextViewerResource(CONTENTS, CONTENTS_HELPER))
       .build();
 
-  @Test
-  void getChars_usesInclusiveIndexing() {
-    var response = resource
-        .client()
-        .target("/chars/4/8")
-        .request()
-        .get();
-
-    assertThat(response.getStatus()).isEqualTo(200);
-
-    var body = response.readEntity(String.class);
-    assertThat(body.length()).isEqualTo(5);
-    assertThat(body).isEqualTo(CONTENTS.asUtf8String().substring(4, 9));
+  @SuppressWarnings("unused") // used as @MethodSource
+  private static Stream<Arguments> provideCasesForChars() {
+    return Stream.of(
+        Arguments.of("/chars/-1/4", 400, ""),
+        Arguments.of("/chars/4/16", 400, ""),
+        Arguments.of("/chars/-1/full", 400, ""),
+        Arguments.of("/chars/full/16", 400, ""),
+        Arguments.of("/chars/8/4", 400, ""),
+        Arguments.of("/chars/4/8", 200, "45678"),
+        Arguments.of("/chars/0/4", 200, "01234"),
+        Arguments.of("/chars/4/15", 200, "456789abcdef"),
+        Arguments.of("/chars/0/full", 200, EXAMPLE),
+        Arguments.of("/chars/full/15", 200, EXAMPLE),
+        Arguments.of("/chars/full/full", 200, EXAMPLE)
+    );
   }
 
-  @Test
-  void getChars_substringWorksForLowIndex() {
-    var response = resource
-        .client()
-        .target("/chars/0/4")
-        .request()
-        .get();
-
-    assertThat(response.getStatus()).isEqualTo(200);
-
-    var body = response.readEntity(String.class);
-    assertThat(body).isEqualTo(CONTENTS.asUtf8String().substring(0, 5));
+  @ParameterizedTest
+  @MethodSource("provideCasesForChars")
+  void getChars_returnsExpectedStatus_and_Contents(String uri, int statusCode, String expected) {
+    final var response = get(uri);
+    assertThat(response.getStatus()).isEqualTo(statusCode);
+    if (statusCode == 200) {
+      final var body = response.readEntity(String.class);
+      assertThat(body).isEqualTo(expected);
+    }
   }
 
-  @Test
-  void getChars_substringWorksForHighIndex() {
-    var response = resource
-        .client()
-        .target("/chars/4/15")
-        .request()
-        .get();
-
-    assertThat(response.getStatus()).isEqualTo(200);
-
-    var body = response.readEntity(String.class);
-    assertThat(body).isEqualTo(CONTENTS.asUtf8String().substring(4));
-  }
-
-  @Test
-  void getChars_understands_full_full_notation() {
-    var response = resource
-        .client()
-        .target("/chars/full/full")
-        .request()
-        .get();
-
-    assertThat(response.getStatus()).isEqualTo(200);
-
-    var body = response.readEntity(String.class);
-    assertThat(body).isEqualTo(CONTENTS.asUtf8String());
+  private Response get(String uri) {
+    return resource.client().target(uri).request().get();
   }
 
   @Test
