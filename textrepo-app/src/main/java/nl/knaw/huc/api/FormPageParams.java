b8120f62334b07998a83696d2e207fa201110a46
HDJ <hayco@users.noreply.github.com>
2021-04-20 10:59:06 +0200

    Document FormPageParams
    although is does not seem to show up in Swagger UI

--- textrepo-app/src/main/java/nl/knaw/huc/api/FormPageParams.java
+++ textrepo-app/src/main/java/nl/knaw/huc/api/FormPageParams.java
@@ -1,6 +1,7 @@
 package nl.knaw.huc.api;
 
 import com.google.common.base.MoreObjects;
+import io.swagger.annotations.ApiModelProperty;
 import nl.knaw.huc.core.Paginated;
 
 import javax.ws.rs.QueryParam;
@@ -8,9 +9,11 @@ import javax.ws.rs.QueryParam;
 public class FormPageParams implements Paginated {
 
   @QueryParam("limit")
+  @ApiModelProperty(value = "how many items to return", example = "20")
   private Integer limit;
 
   @QueryParam("offset")
+  @ApiModelProperty(value = "first item to return", example = "10")
   private Integer offset;
 
   @Override
