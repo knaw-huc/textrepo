75e746e0e163bc497433886251d1119fa9b3fc43
HDJ <hayco@users.noreply.github.com>
2021-04-26 15:38:42 +0200

    Test SimpleXPathResolver

--- textrepo-app/src/test/java/nl/knaw/huc/resources/view/xml/SimpleXPathResolverTest.java
+++ textrepo-app/src/test/java/nl/knaw/huc/resources/view/xml/SimpleXPathResolverTest.java
@@ -6,13 +6,36 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SimpleXPathResolverTest {
 
   @Test
+  void testSimpleResolver_findsNothing_whenNotInFile() throws IOException {
+    final var sut = new SimpleXPathResolver("//NowhereToBeFound");
+    final var result = sut.resolve(someSimpleContents());
+    assertThat(result).isEmpty();
+  }
+
+  @Test
   void testSimpleResolver_findsExcerpt_whenFoundInFile() throws IOException {
+    final var sut = new SimpleXPathResolver("//Creator");
+    final var result = sut.resolve(someSimpleContents());
+    assertThat(result.size()).isEqualTo(1);
+    assertThat(result).containsExactly("<Creator>P2PaLA-PRHLT</Creator>");
+  }
+
+  @Test
+  void testSimpleResolver_findsAll_whenMultiplesInFile() throws IOException {
+    final var sut = new SimpleXPathResolver("//Twice");
+    final var result = sut.resolve(someSimpleContents());
+    assertThat(result.size()).isEqualTo(2);
+    assertThat(result).contains("<Twice>first</Twice>", "<Twice>second</Twice>");
+  }
+
+  private Contents someSimpleContents() throws IOException {
     final var bytes = TestUtils.getResourceAsBytes("xpath/simple.xml");
-    final var contents = Contents.fromBytes(bytes);
-    System.err.printf("Contents: %s%n%n", contents);
+    return Contents.fromBytes(bytes);
   }
 
 }
