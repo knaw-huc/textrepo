1452a307a305cc318c536de29d646e16dac85caa
basl <basleenknegt@gmail.com>
2021-08-09 14:45:31 +0200

    Add link in javadoc to json schema in indexing documentation

--- textrepo-app/src/main/java/nl/knaw/huc/api/FormIndexerType.java
+++ textrepo-app/src/main/java/nl/knaw/huc/api/FormIndexerType.java
@@ -2,6 +2,10 @@ package nl.knaw.huc.api;
 
 import java.util.List;
 
+/**
+ * Note: when changing json format of indexer types,
+ * also change json schema in indexing documentation
+ */
 public class FormIndexerType {
   public String mimetype;
   public List<String> subtypes;
